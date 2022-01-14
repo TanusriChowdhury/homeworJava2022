c352d6fdabc4289d8def6aae551bbcb3b5fc9ba6
Subrata Chowdhury <subratachowdhury@Subratas-MBP.lan>
2022-01-13 22:58:27 -0500

    first commit

---
+++ src/hw1Q2JavaBasics/MySelf.java
@@ -0,0 +1,20 @@
+package hw1Q2JavaBasics;
+
+/*
+* Project names  are generally start with upper case
+* Camel case example for a project -->HomeWorkJava
+* Alternative example of Camel case, for a project -->Home_work_java
+* Package names are generally start with lower case.
+* Camel case example for a package -->hw1Q2JavaBasics
+* Alternative example of Camel case, for a package -->hw1_q2_java_basics
+* Class names start with upper case
+* Camel case example for class -->MySelf
+*Alternative example of Camel case, for a class -->My_self
+    */
+public class MySelf {
+//	Entry point for Java. 
+
+public static void main(String[] args) {
+		System.out.println("My name is Tanusri Chowdhury");
+	}
+}
---
+++ src/hw2Q2JavaBasics/MyCar.java
@@ -0,0 +1,18 @@
+package hw2Q2JavaBasics;
+/*
+ * Create a package name "hw2Q2JavaBasics"[create it by the help of header, not by right click of src file] inside the Homework project created before. Create a Class "MyCar" from the header (not by right click of package, this is the other way to create class). Under main method the first outcome will be: [My name is <your name>, I love to drive, My care info is below:].Use String concatenation feature from above outcome[10]. Now use other syso to print your car each info (color, model, year, wheel drive [example:4WD] etc.[you can add more if you want]) in a separate line [try to follow how to copy and paste a line in IDE by key board command]. Use print instead of println for the car feature when print[20], use \n and \t at least once anywhere in the class[10]. organize code at the end [10], After completing the code, paste it below.
+ */
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		System.out.println(" My name is Tanusri Chowdhury." + " I love to drive." + " My care info is below.");
+		System.out.print("\n\t My Car model is Volkswagen 2022 ID.4 SUV. ");
+		System.out.print(" My car color is Blue .");
+		System.out.print(" It is electric. ");
+		System.out.print(
+				"\n\n \t Going electric isn’t a sacrifice anymore. From the styling of the vehicles themselves to charging access options, it’s an exciting moment for mobility. And the future of electric vehicles is here with the Volkswagen ID.4 electric vehicle. \t Learn more about the latest EV technology, the near-instant torque and acceleration, cost savings, charging options and more. You have questions about moving to electric mobility");
+
+	}
+
+}
